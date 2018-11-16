a47a03f24d5d27adc41f92a9f9789521ff74f461
Petar Filev <petarfilev91@gmail.com>
2018-11-16 17:10:19 +0100

    Add files via upload

---
+++ ThreeDS_SDK.framework/Headers/ThreeDS_SDK.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeDS_SDK.
+FOUNDATION_EXPORT double ThreeDS_SDKVersionNumber;
+
+//! Project version string for ThreeDS_SDK.
+FOUNDATION_EXPORT const unsigned char ThreeDS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeDS_SDK/PublicHeader.h>
